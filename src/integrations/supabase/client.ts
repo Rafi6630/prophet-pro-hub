f6bc2db760365e90b4c7e93347caace3dca05e05
Rafi6630 <111280611+Rafi6630@users.noreply.github.com>
2026-04-25 20:33:18 +0300

    Add files via upload

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://dqylnmkmooabylidbisu.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxeWxubWttb29hYnlsaWRiaXN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1MjE0NjIsImV4cCI6MjA3NDA5NzQ2Mn0.sNKxpfpuM2Ccj_jIJZcfLN3GnPPTDNFoL9dTLD4YRoI";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
